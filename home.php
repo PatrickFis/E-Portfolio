badb7b0eb020d24c828a9b617b34995c04215ac6
Patrick Fischer <fischerpl18@gmail.com>
2017-01-26 16:41:03 -0600

    Updated copyright, moved navbar to other file.

--- home.php
+++ home.php
@@ -11,28 +11,9 @@
 </head>
 <body>
 
-  <nav class="navbar navbar-inverse">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">E-Portfolio</a>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="home.php">Home</a></li>
-          <li><a href="courses.php">Courses</a></li>
-          <li><a href="projects.php">Projects</a></li>
-          <li><a href="extra.php">Extracurriculars</a></li>
-          <li><a href="others.php">Other E-Portfolios</a></li>
-        </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+  <?php
+  	include 'navbar.php';
+  ?>
     <div class="container">
       <div class="page-header">
         <h1>Patrick Fischer</h1>
@@ -66,7 +47,7 @@
   <br><br><br><br><br>
   <div class="navbar navbar-default navbar-fixed-bottom">
       <div class="container">
-        <p class="navbar-text pull-left">© 2016</p>
+        <p class="navbar-text pull-left">© 2017</p>
       </div>
   </div>
   </body>
