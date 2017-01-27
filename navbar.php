b43a23b7abe19a309adcb289afdf35d13d3f8d80
Patrick Fischer <fischerpl18@gmail.com>
2017-01-27 16:06:29 -0600

    Removed others page

--- navbar.php
+++ navbar.php
@@ -15,7 +15,6 @@
           <li><a href="courses.php">Courses</a></li>
           <li><a href="projects.php">Projects</a></li>
           <li><a href="extra.php">Extracurriculars</a></li>
-          <li><a href="others.php">Other E-Portfolios</a></li>
         </ul>
         </div><!--/.nav-collapse -->
       </div>
