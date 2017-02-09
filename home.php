807b22c49422bb4d3aaf61d1e180d24e5d8370a4
Patrick Fischer <fischerpl18@gmail.com>
2017-02-09 16:34:22 -0600

    Test for portfolio stuff

--- home.php
+++ home.php
@@ -12,44 +12,42 @@
 <body>
 
   <?php
-  	include 'navbar.php';
+  include 'navbar.php';
   ?>
-    <div class="container">
-      <div class="page-header">
-        <h1>Patrick Fischer</h1>
-        <p>Most of my work has been on adding a framework to my courses page to use for future work and an outline of what I would like to have on my extracurriculars page.</p>
-      </div>
+  <div class="container">
+    <div class="page-header">
+      <h1>Patrick Fischer</h1>
+      <p>Most of my work has been on adding a framework to my courses page to use for future work and an outline of what I would like to have on my extracurriculars page.</p>
+    </div>
 
+    <div class="container-fluid bg-3 text-center">
+      <h3>Some of my Work</h3><br>
       <div class="row">
-          <ul class="nav nav-tabs-no-style nav-stacked col-md-3">
-            <li class="active"><a data-toggle="tab" href="#Tab1">Tab1</a></li>
-            <li><a data-toggle="tab" href="#Tab2">Tab2</a></li>
-            <li><a data-toggle="tab" href="#Tab3">Tab3</a></li>
-          </ul>
-          <div class="col-md-6">
-            <div class="tab-content">
-              <div id="Tab1" class="tab-pane fade in active">
-                <h3>Tab1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div id="Tab2" class="tab-pane fade">
-                <h3>Tab2</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div id="Tab3" class="tab-pane fade">
-                <h3>Tab3</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-            </div>
-          </div>
+        <div class="col-sm-3">
+          <p>Some text..</p>
+          <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-sm-3">
+          <p>Some text..</p>
+          <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-sm-3">
+          <p>Some text..</p>
+          <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-sm-3">
+          <p>Some text..</p>
+          <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
         </div>
       </div>
+    </div><br>
+  </div>
   <!-- Footer here, can add this to other pages later. -->
   <br><br><br><br><br>
   <div class="navbar navbar-default navbar-fixed-bottom">
-      <div class="container">
-        <p class="navbar-text pull-left">© 2017</p>
-      </div>
+    <div class="container">
+      <p class="navbar-text pull-left">© 2017</p>
+    </div>
   </div>
-  </body>
-  </html>
+</body>
+</html>
