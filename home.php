654c9926625ae3648e2000f6fb9fa6a93aa0aa32
Patrick Fischer <fischerpl18@gmail.com>
2017-02-09 16:56:08 -0600

    Commit of homepage work

--- home.php
+++ home.php
@@ -24,19 +24,19 @@
       <h3>Some of my Work</h3><br>
       <div class="row">
         <div class="col-sm-3">
-          <p>Some text..</p>
+          <p>Compiler Construction</p>
+          <img src="pictures/compiler/compiler.png" href="courses.php" class="img-responsive" style="width:100%" alt="Image">
+        </div>
+        <div class="col-sm-3">
+          <p>Software Engineering</p>
           <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
         </div>
         <div class="col-sm-3">
-          <p>Some text..</p>
+          <p>Automata &amp; Formal Language Theory</p>
           <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
         </div>
         <div class="col-sm-3">
-          <p>Some text..</p>
-          <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-        </div>
-        <div class="col-sm-3">
-          <p>Some text..</p>
+          <p>Object-Oriented Programming</p>
           <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
         </div>
       </div>
