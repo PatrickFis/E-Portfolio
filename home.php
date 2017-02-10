a9322d87a0a43270c6b50e31cfdbd927e878a0c0
Patrick Fischer <fischerpl18@gmail.com>
2017-02-09 18:06:41 -0600

    Added three sentences to homepage for homework

--- home.php
+++ home.php
@@ -42,6 +42,12 @@
         </div>
       </div>
     </div><br>
+    <div class="container-fluid">
+      <p>Three sentences for reflection paper:</p>
+      <p>Academic: I'll write about some of my more difficult classes(like compiler construction).</p>
+      <p>Service: I'll write about service work that I've done through Lipscomb(SALT and some other work).</p>
+      <p>Work: I'll write about my internship at Genetics Associates.</p>
+    </div>
   </div>
 </body>
 </html>
