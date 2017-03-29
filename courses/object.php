7a4d494cc4d5f7d39c5aaf9c6c90b07b5c62ac20
Patrick Fischer <fischerpl18@gmail.com>
2017-03-29 10:22:44 -0500

    Added info to OO

--- courses/object.php
+++ courses/object.php
@@ -14,42 +14,6 @@
   <?php
   	include '../navbar.php';
   ?>
-    <!-- <div class="container">
-      <div class="page-header">
-        <h1>Patrick Fischer</h1>
-      </div>
-
-      <div class="row">
-          <ul class="nav nav-tabs-no-style nav-stacked col-md-3">
-            <li class="active"><a data-toggle="tab" href="#Tab1">Freshman Year</a></li>
-            <li><a data-toggle="tab" href="#Tab2">Sophomore Year</a></li>
-            <li><a data-toggle="tab" href="#Tab3">Junior Year</a></li>
-            <li><a data-toggle="tab" href="#Tab4">Senior Year</a></li>
-          </ul>
-          <div class="col-md-6">
-            <div class="tab-content">
-              <div id="Tab1" class="tab-pane fade in active">
-                <h3>Freshman Year</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div id="Tab2" class="tab-pane fade">
-                <h3>Sophomore Year</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div id="Tab3" class="tab-pane fade">
-                <h3>Junior Year</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div id="Tab4" class="tab-pane fade">
-                <h3>Senior Year</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
-
-
       <!-- Testing portfolio item from Github -->
       <!-- Page Content -->
 <div class="container">
@@ -58,7 +22,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Object-Oriented Programming
-                <small>Item Subheading</small>
+                <!-- <small>Item Subheading</small> -->
             </h1>
         </div>
     </div>
@@ -72,14 +36,14 @@
         </div>
 
         <div class="col-md-4">
-            <h3>Project Description</h3>
-            <p>Project info will go here: Will showcase various projects(Purple America,
-              Guitar Hero, etc.) here. Will try to include compiled code demos.</p>
+            <h3>Purple America</h3>
+            <p>This program creates a graphical representation of the US, and it colors states and counties
+            based on how their population voted in the presidential election.</p>
             <h3>Project Details</h3>
             <ul>
-                <li>Code</li>
-                <li>Function</li>
-                <li>Challenges</li>
+                <li>The project can be downloaded <a href="#">here</a>.</li>
+                <li>To run the project, simply type ...</li>
+                <li>More information</li>
             </ul>
         </div>
 
