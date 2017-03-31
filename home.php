5da175b092884306612cc7d113bd9458935e62a4
Patrick Fischer <fischerpl18@gmail.com>
2017-03-31 15:53:29 -0500

    Commented out linkedin badge

--- home.php
+++ home.php
@@ -49,7 +49,8 @@
       <p>Work: I'll write about my internship at Genetics Associates.</p>
     </div>
   </div> -->
-  <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="patrick-fischer-32a219a1"><a class="LI-simple-link" href='https://www.linkedin.com/in/patrick-fischer-32a219a1?trk=profile-badge'>Patrick Fischer</a></div>
+  <!-- LinkedIn badge -->
+  <!-- <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="patrick-fischer-32a219a1"><a class="LI-simple-link" href='https://www.linkedin.com/in/patrick-fischer-32a219a1?trk=profile-badge'>Patrick Fischer</a></div> -->
 </body>
 
 <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
