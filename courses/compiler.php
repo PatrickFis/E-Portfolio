cbb197b4815b8ba7e55a972d2a12b16f4ad0f902
Patrick Fischer <fischerpl18@gmail.com>
2017-03-30 20:56:20 -0500

    Added pictures to compiler page

--- courses/compiler.php
+++ courses/compiler.php
@@ -68,7 +68,60 @@
     <div class="row">
 
         <div class="col-md-8">
-            <img class="img-responsive" src="../pictures/compiler/compiler.png" alt="">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="../pictures/compiler/compiler.png" alt="Portion of parser">
+                <div class="carousel-caption">
+                  <h3>Bison Parser</h3>
+                  <p>A short snippet of code taken from the parser</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="../pictures/compiler/zeller.png" alt="Zeller's Congruence SLIC">
+                <div class="carousel-caption">
+                  <h3>Zeller's Congruence</h3>
+                  <p>A small portion of SLIC code from a program that calculates Zeller's Congruence</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="../pictures/compiler/zellergstal.png" alt="Zeller's Congruence GSTAL">
+                <div class="carousel-caption">
+                  <h3>Zeller's Congruence</h3>
+                  <p>Part of the GSTAL code generated from the program shown in the previous slide</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="../pictures/compiler/zelleroutput.png" alt="Zeller's Congruence Output">
+                <div class="carousel-caption">
+                  <h3>Zeller's Congruence</h3>
+                  <p>Output of the assembled program outputing what day a specific date fell on</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+            </div>
         </div>
 
         <div class="col-md-4">
