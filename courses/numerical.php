fad2625d90e1b845447e944c61f4632da9a7900c
Patrick Fischer <fischerpl18@gmail.com>
2017-04-04 14:12:48 -0500

    Added numerical methods stuff

---
+++ courses/numerical.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Numerical Methods</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <?php
+  	include '../navbar.php';
+  ?>
+
+      <!-- Testing portfolio item from Github -->
+      <!-- Page Content -->
+<div class="container">
+
+    <!-- Portfolio Item Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Numerical Methods
+                <small>Item Subheading</small>
+            </h1>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Portfolio Item Row -->
+    <div class="row">
+
+      <div class="col-md-8">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="../pictures/numerical/gauss.png" alt="Portion of code for Gaussian Elimination">
+              <div class="carousel-caption">
+                <h3>Gaussian Elimination</h3>
+                <p>Part of the code used for Gaussian Elimination</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <img src="../pictures/numerical/newton.png" alt="Newton Interpolation Polynomial">
+              <div class="carousel-caption">
+                <h3>Newtom Interpolation</h3>
+                <p>Calculates an interpolation polynomial to estimate a function</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <img src="../pictures/numerical/spline.png" alt="Cubic Spline Function">
+              <div class="carousel-caption">
+                <h3>Cubic Spline</h3>
+                <p>Calculates the coefficients of a cubic spline function</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <img src="../pictures/compiler/zelleroutput.png" alt="Zeller's Congruence Output">
+              <div class="carousel-caption">
+                <h3>Zeller's Congruence</h3>
+                <p>Output of the assembled program showing what day a specific date fell on</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+          </div>
+      </div>
+
+        <div class="col-md-4">
+            <h3>Numerical Methods Projects</h3>
+            <p>TODO</p>
+            <h3>Project Details</h3>
+            <ul>
+                <li>TODO</li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- /.row -->
+
+    <!-- Related Projects Row -->
+    <div class="row">
+
+        <div class="col-lg-12">
+            <h3 class="page-header">Related Projects</h3>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+    </div>
+    <!-- /.row -->
+  <!-- Footer here, can add this to other pages later. -->
+  <!-- <br><br><br><br><br>
+  <div class="navbar navbar-default navbar-fixed-bottom">
+      <div class="container">
+        <p class="navbar-text pull-left">© 2017</p>
+      </div>
+  </div> -->
+  </body>
+  </html>
