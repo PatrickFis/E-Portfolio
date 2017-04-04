4f58b713dd37ec6b4c35b4b309bcb58977701a38
Patrick Fischer <fischerpl18@gmail.com>
2017-04-04 13:26:10 -0500

    Working on homepage

--- home.php
+++ home.php
@@ -14,8 +14,8 @@
   <?php
   include 'navbar.php';
   ?>
-  <div class="jumbotron" >
-    <div class="container text-center">
+  <div class="jumbotron" style="background-color: lightblue !important">
+    <div class="container text-center" style="background-color: lightblue !important">
       <h1>Patrick Fischer</h1>
       <p>Under Construction...</p>
     </div>
