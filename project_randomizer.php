2da3b54ce0e238963a4f9a89f8ef8a665283e561
Patrick Fischer <fischerpl18@gmail.com>
2017-04-04 23:25:09 -0400

    Fixing issues with misnamed files

--- project_randomizer.php
+++ project_randomizer.php
@@ -13,7 +13,7 @@
                     '/courses/object.php', '/courses/operatingsystems.php', '/courses/softwareeng.php',
                     '/extra/ccdc.php', '/extra/genetics.php', '/extra/icpc.php');
   // Contains the pictures used for each project. MUST BE IN THE SAME ORDER AS THE ABOVE ARRAY.
-  $pictures = array('/pictures/automata/automata.jpg', '/pictures/compiler/compiler.jpg', 'http://placehold.it/500x300',
+  $pictures = array('/pictures/automata/automata.png', '/pictures/compiler/compiler.png', 'http://placehold.it/500x300',
                     '/pictures/infosec/pic1.png', '/pictures/mobile_distributed/mobile.png', '/pictures/numerical/gauss.png',
                     '/pictures/object_oriented/object.png', 'http://placehold.it/500x300', 'http://placehold.it/500x300',
                     'http://placehold.it/500x300', 'http://placehold.it/500x300', '/pictures/icpc/acm.jpg');
