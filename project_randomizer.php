6001a2f855014bd39bab5ada59fa51063f70c34d
Patrick Fischer <fischerpl18@gmail.com>
2017-04-04 23:10:59 -0400

    Added project randomizer

---
+++ project_randomizer.php
@@ -0,0 +1,42 @@
+<?php
+  // Produces random links to various projects
+
+  echo '<div class="row">';
+
+  echo '<div class="col-lg-12">';
+  echo '<h3 class="page-header">';
+  echo '</div>';
+
+  // Contains the projects that will be randomly selected and placed at the end of certain pages.
+  $projects = array('/courses/automata.php', '/courses/compiler.php', '/courses/datastruct.php',
+                    '/courses/infosec.php', '/courses/mobile.php', '/courses/numerical.php',
+                    '/courses/object.php', '/courses/operatingsystems.php', '/courses/softwareeng.php',
+                    '/extra/ccdc.php', '/extra/genetics.php', '/extra/icpc.php');
+  // Contains the pictures used for each project. MUST BE IN THE SAME ORDER AS THE ABOVE ARRAY.
+  $pictures = array('/pictures/automata/automata.jpg', '/pictures/compiler/compiler.jpg', 'http://placehold.it/500x300',
+                    '/pictures/infosec/pic1.png', '/pictures/mobile_distributed/mobile.png', '/pictures/numerical/gauss.png',
+                    '/pictures/object_oriented/object.png', 'http://placehold.it/500x300', 'http://placehold.it/500x300',
+                    'http://placehold.it/500x300', 'http://placehold.it/500x300', '/pictures/icpc/acm.jpg');
+
+  $chosenArray = array();
+  for($i = 0; $i < count($projects); $i++) {
+    if(count($chosenArray) < 4) {
+      $randInt = random(0, count($projects-1));
+      if(inArray($chosenArray, $randInt)) {
+        i--;
+      }
+      else {
+        array_push($chosenArray, $randInt);
+      }
+    }
+  }
+
+  for($i = 0; $i < count($chosenArray); $i++) {
+    echo '<div class="col-sm-3 col-xs-6">';
+    echo '<a href="'.$projects[$chosenArray[i]].'">';
+    echo '<img class="img-responsive portfolio-item" src='.$pictures[$chosenArray[i]].'alt=""';
+    echo '</a>';
+    echo '</div>';
+  }
+  echo '</div>';
+ ?>
