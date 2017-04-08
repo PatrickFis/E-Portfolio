9e2b6fa6a1d138dff2935849f50a9e31640c0532
Patrick Fischer <fischerpl18@gmail.com>
2017-04-08 11:43:00 -0500

    Removed subheading from mobile and distributed

--- courses/mobile.php
+++ courses/mobile.php
@@ -22,7 +22,6 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Mobile &amp; Distributed Computing
-                <small>Item Subheading</small>
             </h1>
         </div>
     </div>
