e05285c78bc83e77e77951b21e10292dcde05211
Patrick Fischer <fischerpl18@gmail.com>
2017-04-07 20:50:59 -0500

    Working on the home page

--- project_randomizer.php
+++ project_randomizer.php
@@ -41,7 +41,7 @@
   for($i = 0; $i < count($chosenArray); $i++) {
     echo '<div class="col-sm-3 col-xs-6">';
     echo '<a href="'.$projects[$chosenArray[$i]].'">';
-    echo '<img class="img-responsive portfolio-item" style="height:250px;width:250px;" src="'.$pictures[$chosenArray[$i]].'" alt="">';
+    echo '<img class="img-responsive portfolio-item" style="height:200px;width:200px;" src="'.$pictures[$chosenArray[$i]].'" alt="">';
     echo '</a>';
     echo '</div>';
   }
