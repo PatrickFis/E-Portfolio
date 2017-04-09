462ea0f81a467a26a362e88461d8caef17a55bd9
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 08:34:35 -0500

    Working on portfolio

--- testportfolio.php
+++ testportfolio.php
@@ -90,20 +90,6 @@
     </section>
 
     <section class="no-padding" id="portfolio">
-      <div class="container-fluid">
-        <div class="row no-gutter popup-gallery">
-          <div class="col-lg-4 col-sm-6">
-            <a href="pictures/seccdc/group_pic.jpg" class="portfolio-box">
-            <img src="pictures/seccdc/group_pic.jpg" class="img-response">
-            <div class="portfolio-box-caption-content">
-              <div class="project-category text-faded">
-                SECCDC
-              </div>
-            </div>
-          </a>
-          </div>
-        </div>
-      </div>
         <div class="container-fluid">
             <div class="row no-gutter popup-gallery">
                 <div class="col-lg-4 col-sm-6">
@@ -169,6 +155,21 @@
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/5.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/fullsize/6.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
