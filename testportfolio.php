422bc21ec4e32d5cf4ebef8e1ae6c2f1f51f3ca7
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 09:31:01 -0500

    Testing hover

--- testportfolio.php
+++ testportfolio.php
@@ -45,7 +45,7 @@
       bottom: 0;
       left: 0;
       right: 0;
-      background: rgba(255, 255, 255, 0.72);
+      background: rgba(255, 255, 255, 0.00);
       color: black;
       visibility: hidden;
       opacity: 0;
