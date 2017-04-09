9fcf63486a7e92bcb4ca5f5e7017d3fbd3c67702
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 11:14:20 -0500

    Working on new navbar

--- courses/automata.php
+++ courses/automata.php
@@ -22,7 +22,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body id="page-top">
 
   <?php
   	include '../new_navbar.php';
