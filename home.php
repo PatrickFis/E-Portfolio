ef5af42d8cee273d1745af13cdd14c15a6de39f4
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 08:06:06 -0500

    Working on homepage

--- home.php
+++ home.php
@@ -249,7 +249,7 @@
                     <hr class="primary">
                     <p>Please contact me on LinkedIn</p>
                 </div>
-                <div class="col-lg-6 col-lg-offset-4 text-center">
+                <div class="col-lg-4 col-lg-offset-4">
                     <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="patrick-fischer-32a219a1"><a class="LI-simple-link" href='https://www.linkedin.com/in/patrick-fischer-32a219a1?trk=profile-badge'>Patrick Fischer</a></div>
                 </div>
                 <!-- <div class="col-lg-4 text-center">
