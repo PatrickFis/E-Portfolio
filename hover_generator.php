05e14e0ee9b3051d974a5eec7b93ba20cf6fc78e
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 16:27:46 -0500

    Rewrote hover generator...

--- hover_generator.php
+++ hover_generator.php
@@ -38,9 +38,6 @@
   }
   // Page content
   echo '<div class="container-fluid">';
-  // Row 1
-  echo '<div class="row">';
-  // col 1
   for($i = 0; $i < 2; $i++) {
     echo '<div class="row">';
     for($j = 0; $j < 3; $j++) {
@@ -68,93 +65,6 @@
     }
     echo '</div>';
   }
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[0]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[0]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[0]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  //
-  // //col 2
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[1]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[1]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[1]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  //
-  // //col 3
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[2]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[2]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[2]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // // Row 2
-  // echo '<div class="row">';
-  //
-  // // col 1
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[3]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[3]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[3]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  //
-  // //col 2
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[4]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[4]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[4]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  //
-  // //col 3
-  // echo '<div class="col-lg-4 col-sm-6">';
-  // echo '<div class-"portfolio-box">';
-  // echo '<a href="'.$projects[$chosenArray[5]].'" class="portfolio-box">';
-  // echo '<img src="'.$pictures[$chosenArray[5]].'" class="img-responsive">';
-  // echo '<div class="portfolio-box-caption">';
-  // echo '<div class="portfolio-box-caption-content">';
-  // echo '<div class="project-category text-faded">'.$descriptions[$chosenArray[5]].'</div>';
-  // echo '</div>';
-  // echo '</div>';
-  // echo '</a>'; // link
-  // echo '</div>';
-  // echo '</div>';
-  //
-  // echo '</div>';
 
   echo '</div>';
  ?>
