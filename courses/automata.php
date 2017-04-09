2285877bf9eb4554c34bf7e2c13e925543a9c7d1
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 11:19:42 -0500

    Working on new navbar

--- courses/automata.php
+++ courses/automata.php
@@ -18,7 +18,7 @@
   <!-- <link href="/vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> -->
 
   <!-- Theme CSS -->
-  <!-- <link href="/css/creative.min.css" rel="stylesheet"> -->
+  <link href="/css/creative.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
