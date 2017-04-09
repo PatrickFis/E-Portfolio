208effb8db054f6fc6fa2f91707909194a88125a
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 11:07:26 -0500

    Working on new navbar

---
+++ new_navbar.php
@@ -0,0 +1,44 @@
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">Patrick Fischer's E-Portfolio</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="testportfolio.php#about">About</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Contact</a>
+                </li>
+                <li class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Courses
+                  <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="/courses/object.php">Object-Oriented Programming</a></li>
+                    <li><a href="/courses/automata.php">Automata &amp; Formal Language Theory</a></li>
+                    <li><a href="/courses/compiler.php">Compiler Construction</a></li>
+                    <li><a href="/courses/mobile.php">Mobile &amp; Distributed Computing</a></li>
+                    <li><a href="/courses/infosec.php">Information Security</a></li>
+                    <li><a href="/courses/numerical.php">Numerical Methods</a></li>
+                    <li><a href="/courses/operatingsystems.php">Operating Systems</a></li>
+                    <li><a href="/courses/softwareeng.php">Software Engineering</a><li>
+                    <li><a href="/courses/datastruct.php">Data Structures</a><li>
+                    <!-- <li><a href="/courses/courses.php">Software Engineering</a></li> -->
+                  </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
