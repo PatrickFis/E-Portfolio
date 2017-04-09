4a73d17d444588c068ff98df9e77ed03e9c42ce5
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 12:41:36 -0500

    Trying to fix mobile dropdown

--- home.php
+++ home.php
@@ -24,7 +24,11 @@
 
     <!-- Theme CSS -->
     <link href="css/creative.min.css" rel="stylesheet">
-
+    <style>
+      .dropdown-backdrop {
+        position: static;
+      }
+    </style>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
