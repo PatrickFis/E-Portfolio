9df764ea47941f7fc745d6d68ee0ad766f83ca10
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 08:31:17 -0500

    Working on portfolio

--- testportfolio.php
+++ testportfolio.php
@@ -92,7 +92,7 @@
     <section class="no-padding" id="portfolio">
       <div class="container-fluid">
         <div class="row no-gutter">
-          <div class="col-lg-4 col-sm-6">
+          <div class="col-lg-4 col-sm-6" class="portfolio-box">
             <img src="pictures/seccdc/group_pic.jpg" class="img-response">
             <div class="portfolio-box-caption-content">
               <div class="project-category text-faded">
