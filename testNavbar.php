b0d6a89bd89104914276303342bb72ed0d143041
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 11:37:14 -0500

    Working on new navbar

--- testNavbar.php
+++ testNavbar.php
@@ -23,91 +23,22 @@
     <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
     <!-- Theme CSS -->
-    <!-- <link href="css/creative.min.css" rel="stylesheet"> -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
     <style>
-    .navbar-default {
-      background-color: white;
-      border-color: rgba(34, 34, 34, 0.05);
-      font-family: 'Open Sans', 'Helvetica Neue', Arial, sans-serif;
-      -webkit-transition: all 0.35s;
-      -moz-transition: all 0.35s;
-      transition: all 0.35s;
+    header {
+      position: relative;
+      width: 100%;
+      min-height: auto;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      background-size: cover;
+      -o-background-size: cover;
+      background-position: center;
+      background-image: url('../img/header.jpg');
+      text-align: center;
+      color: white;
     }
-    .navbar-default .navbar-header .navbar-brand {
-      color: #F05F40;
-      font-family: 'Open Sans', 'Helvetica Neue', Arial, sans-serif;
-      font-weight: 700;
-      text-transform: uppercase;
-    }
-    .navbar-default .navbar-header .navbar-brand:hover,
-    .navbar-default .navbar-header .navbar-brand:focus {
-      color: #eb3812;
-    }
-    .navbar-default .navbar-header .navbar-toggle {
-      font-weight: 700;
-      font-size: 12px;
-      color: #222222;
-      text-transform: uppercase;
-    }
-    .navbar-default .nav > li > a,
-    .navbar-default .nav > li > a:focus {
-      text-transform: uppercase;
-      font-weight: 700;
-      font-size: 13px;
-      color: #222222;
-    }
-    .navbar-default .nav > li > a:hover,
-    .navbar-default .nav > li > a:focus:hover {
-      color: #F05F40;
-    }
-    .navbar-default .nav > li.active > a,
-    .navbar-default .nav > li.active > a:focus {
-      color: #F05F40 !important;
-      background-color: transparent;
-    }
-    .navbar-default .nav > li.active > a:hover,
-    .navbar-default .nav > li.active > a:focus:hover {
-      background-color: transparent;
-    }
-      .navbar-default {
-        background-color: transparent;
-        border-color: rgba(255, 255, 255, 0.3);
-      }
-      .navbar-default .navbar-header .navbar-brand {
-        color: rgba(255, 255, 255, 0.7);
-      }
-      .navbar-default .navbar-header .navbar-brand:hover,
-      .navbar-default .navbar-header .navbar-brand:focus {
-        color: white;
-      }
-      .navbar-default .nav > li > a,
-      .navbar-default .nav > li > a:focus {
-        color: rgba(255, 255, 255, 0.7);
-      }
-      .navbar-default .nav > li > a:hover,
-      .navbar-default .nav > li > a:focus:hover {
-        color: white;
-      }
-      .navbar-default.affix {
-        background-color: white;
-        border-color: rgba(34, 34, 34, 0.05);
-      }
-      .navbar-default.affix .navbar-header .navbar-brand {
-        color: #F05F40;
-        font-size: 14px;
-      }
-      .navbar-default.affix .navbar-header .navbar-brand:hover,
-      .navbar-default.affix .navbar-header .navbar-brand:focus {
-        color: #eb3812;
-      }
-      .navbar-default.affix .nav > li > a,
-      .navbar-default.affix .nav > li > a:focus {
-        color: #222222;
-      }
-      .navbar-default.affix .nav > li > a:hover,
-      .navbar-default.affix .nav > li > a:focus:hover {
-        color: #F05F40;
-      }
     </style>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -165,11 +96,11 @@
         <!-- /.container-fluid -->
     </nav>
 
-    <!-- <header>
+    <header>
         <div class="header-content hidden">
 
         </div>
-    </header> -->
+    </header>
 
     <!-- jQuery -->
     <script src="vendor/jquery/jquery.min.js"></script>
