3ae49cb4f13ca3443feeaed5ab31268a8093f996
Patrick Fischer <fischerpl18@gmail.com>
2017-04-09 17:00:02 -0500

    Alphabetized navbar

--- new_navbar.php
+++ new_navbar.php
@@ -24,18 +24,17 @@
                   <a class="dropdown-toggle" data-toggle="dropdown" href="#">Courses
                   <span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="/courses/object.php">Object-Oriented Programming</a></li>
                     <li><a href="/courses/automata.php">Automata &amp; Formal Language Theory</a></li>
                     <li><a href="/courses/compiler.php">Compiler Construction</a></li>
-                    <li><a href="/courses/mobile.php">Mobile &amp; Distributed Computing</a></li>
-                    <li><a href="/courses/infosec.php">Information Security</a></li>
-                    <li><a href="/courses/numerical.php">Numerical Methods</a></li>
-                    <li><a href="/courses/operatingsystems.php">Operating Systems</a></li>
-                    <li><a href="/courses/softwareeng.php">Software Engineering</a></li>
                     <li><a href="/courses/datastruct.php">Data Structures</a></li>
                     <li><a href="/courses/dbms.php">Database Management Systems</a></li>
                     <li><a href="/courses/design.php">Design &amp; Analysis of Algorithms</a></li>
-                    <!-- <li><a href="/courses/courses.php">Software Engineering</a></li> -->
+                    <li><a href="/courses/infosec.php">Information Security</a></li>
+                    <li><a href="/courses/mobile.php">Mobile &amp; Distributed Computing</a></li>
+                    <li><a href="/courses/numerical.php">Numerical Methods</a></li>
+                    <li><a href="/courses/object.php">Object-Oriented Programming</a></li>
+                    <li><a href="/courses/operatingsystems.php">Operating Systems</a></li>
+                    <li><a href="/courses/softwareeng.php">Software Engineering</a></li>
                   </ul>
                 </li>
                 <li class="dropdown">
